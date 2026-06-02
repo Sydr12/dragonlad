dc27e9cc4b9a2312f6f13d0022e61d1d3936d9ce
Sydr12 <sanada.moon@gmail.com>
2026-06-03 00:23:27 +0900

    Redeploy

---
+++ _next/static/BON_mCKfJu95dF0xKprvb/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Finfo\u002Fequips\u002F[id]","\u002Finfo\u002Fheroes\u002F[id]","\u002Finfo\u002Fskills\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
